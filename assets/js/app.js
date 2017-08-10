117ae4510e9e4d1b780cb5f63bdfcc42e5185f16
dane kilpatrick <dkilpatrick31>
2017-08-10 14:42:59 -0500

    Template is done for the most part.

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+console.log("Welcome to my page! -Dane");
+
+$( document ).ready(function() {
+
+  $('a[href*=\\#]').on('click', function(event){
+      event.preventDefault();
+      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+  });
+
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+  $(function() {
+    $(document).scroll(function() {
+      var x = $(this).scrollTop(),
+        $nav = $(".navbar-fixed-top");
+      if (x > 4300) {
+        $nav.removeClass('scrolled');
+      }
+    });
+  });
+
+});
